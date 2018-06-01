3caddf7854581d133784e19bc81ab7cfc3a3e317
Olivia R Hardwidge <oh72183@srcs.loc>
2018-06-01 11:43:49 -0700

    hi

--- animation.js
+++ animation.js
@@ -1,9 +1,8 @@
-var box = document.getElementById("animationbird");
+var box = document.getElementById("animatebird");
 var xpos = 0;
 var ypos = 0;
 var xstep = 1;
 var ystep = 1;
-var id = setInterval(frame, 5);
 
 function frame() {
     if (xpos > 350 || xpos < 0) {
@@ -13,8 +12,27 @@ function frame() {
         ystep = -ystep;
     }
 
+
     xpos = xpos + xstep;
     ypos = ypos + ystep;
     box.style.top = ypos + 'px';
     box.style.left = xpos + 'px';
+}
+
+var mousecoords = document.getElementById("mousecoords");
+var clickcolor = document.getElementById("clickcolor");
+
+document.addEventListener("mousemove", printMouseCoords);
+document.addEventListener("click", colorChange);
+
+function printMouseCoords(event) {
+    var mouseX = event.clientX;
+    var mouseY = event.clientY;
+    document.getElementById("animatebird").style.left = mouseX + "px";
+    document.getElementById("animatebird").style.top = mouseY + "px";
+}
+
+function colorChange() {
+    clickcolor.style.color = "pink";
+
 }
